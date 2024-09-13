c9fcc2e1133533dd2bab4983d8464bbd87684198
Antonio Santos <antonio.santos@runtime-revolution.com>
2024-09-13 09:48:16 +0100

    Add tag resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_12_164344) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_13_084541) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_12_164344) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
